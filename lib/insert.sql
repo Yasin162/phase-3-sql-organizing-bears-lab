26afc7a31f7d040b91a7b25965689e6ce3923ace
Yasin <yasinabdussaburr@gmail.com>
2021-11-09 09:58:28 -0500

    Assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, alive)
+VALUES ("Mr. Chocolate", 4, "M", "yellow", true),
+("Rowdy", 2, "M", "brown", false),
+("Tabitha", 9, "M", "blue", true),
+("Sergeant Brown", 74, "M", "purple", false),
+("Melissa", 83, "F", "purple", true),
+("Grinch", 45, "M", "green", true),
+("Wendy", 23, "F", "red", true),
+(NULL, 99, Null, NULL, true);
